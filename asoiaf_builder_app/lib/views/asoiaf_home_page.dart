debaa296d91ebdb32ed677698c68b37a9ec16113
Jordan Soubrier <jordansoubrier@hotmail.fr>
2021-05-22 02:21:51 +0200

    Starting creation of pages

---
+++ asoiaf_builder_app/lib/views/asoiaf_home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'houses_page.dart';
+
+// ASOAIF Home page class
+class AsoiafHomePage extends StatefulWidget {
+  AsoiafHomePage({Key key}) : super(key: key);
+
+  @override
+  _AsoiafHomePageState createState() => _AsoiafHomePageState();
+}
+
+class _AsoiafHomePageState extends State<AsoiafHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/background.jpg'),
+          fit: BoxFit.cover
+        )
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: 
+          <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 40, 0, 10),
+              child: Image.asset('images/logo.png')
+            ),
+            Expanded(
+              flex: 9,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HousesPage())
+                      );
+                    }, 
+                    child: Text('Houses')
+                  ),
+                ],
+              )
+            ),
+          ]
+      )
+    );
+  }
+}
---
+++ asoiaf_builder_app/lib/views/houses_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// Houses page
+class HousesPage extends StatefulWidget {
+  HousesPage({Key key}) : super(key: key);
+
+  @override
+  _HousesPageState createState() => _HousesPageState();
+}
+
+class _HousesPageState extends State<HousesPage> {
+  @override
+  Widget build(Object context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
